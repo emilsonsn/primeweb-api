5900b4bcb0b266813adff010408ac9e9e62b0d8e
emilsonsn <emilsonsn2@gmail.com>
2024-11-06 10:00:21 -0300

    feat: adiciona campo de número do endereço em usuários

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable implements JWTSubject
         'phone',
         'cep',
         'street',
+        'number',
         'neighborhood',
         'city',
         'state',
